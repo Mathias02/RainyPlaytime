8dd3a064df2cac9d538049261b90f5df99139f30
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-06-29 22:25:50 +0100

    html search bar

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,17 @@ import "./Weather.css";
 
  export default function Weather() {
     return (
-        <div className="Weather">Weather Page</div>
+       <div className="Weather">
+           <div class="input-group"> 
+               <input type="text" id="" name="" class="form-control" value="Enter City"/>
+                 <div class="input-group-append"> 
+                   <button type="button" class="btn waves-effect waves-light btn-custom">
+                    <i class="fa fa-search mr-1"> </i> Find Weather</button>
+                 </div>
+            </div>
+
+        </div>
+       
     )
 } 
 
